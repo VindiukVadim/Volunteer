27bd98be900696190a4ce632685c33ec91d468c0
Vadym <agentvinjukv1@gmail.com>
2023-05-03 17:22:26 +0200

    fix

--- Volunteer/Data/Migrations/20230502211917_init.Designer.cs
+++ Volunteer/Data/Migrations/20230502211917_init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volunteer.Data;
 
@@ -11,9 +12,11 @@ using Volunteer.Data;
 namespace Volunteer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230502211917_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
